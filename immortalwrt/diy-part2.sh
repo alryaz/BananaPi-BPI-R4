d81cc3517951a27078e8d0b84e31c1a97030be7f
Ryazanov Alexander Mihailovich <alryaz@alryaz.com>
2026-01-25 15:08:12 +0300

    refactor: reorganize rust builds

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 #
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' \
+  package/base-files/files/bin/config_generate
+
+#
+# Fix rust build
+sed -i 's/llvm.download-ci-llvm=true/llvm.download-ci-llvm=false/g' \
+  packages/‎lang/rust/Makefile‎
